8fc80edfe9c27eb015a6bb9b4da2a314f4c954e2
Pablo SN <pablo.silvana@udlap.mx>
2026-06-23 18:13:38 -0700

    dios mio recógeme por favor

---
+++ data_exploring.py
@@ -0,0 +1,13 @@
+import panel_builder as pb
+import panel_regressions_1st as p
+import src.data_wrangling as dw
+import src.data_loader as dl
+import event_study as es
+import pandas as pd
+
+#%%
+
+from src.data_loader import austin_crime
+from src.data_loader import  austin_311_public
+
+#%%
